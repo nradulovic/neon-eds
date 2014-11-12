d54fcd509a52b53771a23f9c418d5fc4bbff1427
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2014-11-12 16:21:40 +0100

    New GPIO and I2C interfaces

---
+++ include/ngpio.h
@@ -0,0 +1,45 @@
+#ifndef NGPIO_H_
+#define NGPIO_H_
+
+#include <stdbool.h>
+#include "plat/gpio.h"
+
+#ifndef GPIO_PIN_ID
+#define GPIO_PIN_ID(port, pin)          ((port) * PORT_PINS_PER_GPIO + (pin))
+#endif
+
+#define GPIO_INPUT                      (0x0ul << 0)
+#define GPIO_OUTPUT                     (0x1ul << 0)
+#define GPIO_OUTPUT_OPEN_DRAIN          (0x2ul << 0)
+#define GPIO_PULL_UP                    (0x1ul << 2)
+#define GPIO_PULL_DOWN                  (0x2ul << 2)
+
+struct gpio_port;
+
+struct gpio_pin_config 
+{
+    uint32_t                    flags;
+};
+
+struct gpio_pin
+{
+    struct gpio_port *          port;
+    uint32_t                    pin_mask;
+}
+
+void gpio_driver_init(void);
+
+void gpio_init(struct gpio_port * port, uint32_t pin_no, const struct gpio_pin_config * config);
+void gpio_term(struct gpio_port * port, uint32_t pin_no);
+
+void gpio_pin_init(uint32_t pin_id, const struct gpio_pin_config * config);
+void gpio_pin_term(uint32_t pin_id);
+bool gpio_get(uint32_t pin_id);
+void gpio_set(uint32_t pin_id);
+void gpio_clear(uint32_t pin_id);
+bool gpio_request(uint32_t pin_id);
+void gpio_release(uint32_t pin_id);
+bool gpio_change_notice_request(uint32_t pin_id, void (* change_handler)(uint32_t pin_id));
+bool gpio_change_notice_release(uint32_t pin_id);
+
+#define /* NGPIO_H_ */
---
+++ include/ni2c.h
@@ -0,0 +1,81 @@
+#ifndef NI2C_H_
+#define NI2C_H_
+
+#include "plat/i2c.h"
+
+#define I2C_BUS_ADDRESS_7BIT            (0x0u << 0)
+#define I2C_BUS_ADDRESS_10BIT           (0x1u << 0)
+
+#define I2C_SLAVE_RD_NACK               (0x1u << 0)
+#define I2C_SLAVE_RD_START_STOP         (0x1u << 1)
+#define I2C_SLAVE_RD_REPEAT             (0x2u << 1)
+
+struct i2c_bus_config
+{
+    uint32_t                    flags;
+    uint32_t                    speed;
+    uint32_t                    isr_prio;
+};
+
+struct i2c_bus;
+
+struct i2c_slave_config
+{
+    uint32_t                    flags;
+    uint8_t                     address;
+};
+
+struct i2c_slave
+{
+    struct i2c_bus *            bus;
+    uint32_t                    flags;
+    uint8_t                     address;
+};
+
+void i2c_driver_init(void);
+
+void i2c_bus_init(
+    struct i2c_bus *            bus, 
+    const struct i2c_bus_config * config);
+    
+void i2c_bus_term(
+    struct i2c_bus *            bus);
+    
+void i2c_bus_ack(
+    struct i2c_bus *            bus);
+    
+void i2c_bus_nack(
+    struct i2c_bus *            bus);
+    
+bool i2c_bus_write_array(
+    struct i2c_bus *            bus,
+    const uint8_t *             data,
+    size_t                      size);
+
+bool i2c_bus_write(
+    struct i2c_bus *            bus,
+    uint8_t                     data);
+
+void i2c_bus_read_array(
+    struct i2c_bus *            bus,
+    uint8_t *                   data,
+    size_t                      size);
+
+uint8_t i2c_bus_read(
+    struct i2c_bus *            bus);
+
+void i2c_bus_start(
+    struct i2c_bus *            bus);
+
+void i2c_bus_restart(
+    struct i2c_bus *            bus);
+
+void i2c_bus_stop(
+    struct i2c_bus *            bus);
+
+void i2c_slave_open(struct i2c_slave * slave, const struct i2c_slave_config * config, struct i2c_bus * bus, uint8_t id);
+void i2c_slave_close(struct i2c_slave * slave);
+bool i2c_slave_read(struct i2c_slave * slave, uint8_t address, void * data, size_t size);
+bool i2c_slave_write(struct i2c_slave * slave, uint8_t address, const void * data, size_t size);
+
+#endif /* NI2C_H_ */
