cfaf7dcc088fbf261097ac689506e2196555977c
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2015-04-07 03:16:39 +0200

    Added template source files

---
+++ documentation/template/header_template.h
@@ -0,0 +1,51 @@
+/*
+ * This file is part of Neon.
+ *
+ * Copyright (C) 2010 - 2015 Nenad Radulovic
+ *
+ * Neon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Neon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Neon.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * web site:    http://github.com/nradulovic
+ * e-mail  :    nenad.b.radulovic@gmail.com
+ *//***********************************************************************//**
+ * @file
+ * @author      {author name}
+ * @brief       {Brief description header}
+ * @defgroup    {def_group Name}
+ * @brief       {Brief description}
+ *********************************************************************//** @{ */
+
+#ifndef NEON_HEADER_TEMPLATE_H_
+#define NEON_HEADER_TEMPLATE_H_
+
+/*=========================================================  INCLUDE FILES  ==*/
+/*===============================================================  MACRO's  ==*/
+/*-------------------------------------------------------  C++ extern base  --*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*============================================================  DATA TYPES  ==*/
+/*======================================================  GLOBAL VARIABLES  ==*/
+/*===================================================  FUNCTION PROTOTYPES  ==*/
+/*--------------------------------------------------------  C++ extern end  --*/
+#ifdef __cplusplus
+}
+#endif
+
+/*================================*//** @cond *//*==  CONFIGURATION ERRORS  ==*/
+/** @endcond *//** @} *//******************************************************
+ * END of header_template.h
+ ******************************************************************************/
+#endif /* NEON_HEADER_TEMPLATE_H_ */
---
+++ documentation/template/source_template.c
@@ -0,0 +1,43 @@
+/*
+ * This file is part of Neon.
+ *
+ * Copyright (C) 2010 - 2015 Nenad Radulovic
+ *
+ * Neon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Neon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Neon.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * web site:    http://github.com/nradulovic
+ * e-mail  :    nenad.b.radulovic@gmail.com
+ *//***********************************************************************//**
+ * @file
+ * @author      {author name}
+ * @brief       {brief description}
+ *********************************************************************//** @{ */
+/**@defgroup    {def_impl Implementation}
+ * @brief       {Implementation}
+ * @{ *//*--------------------------------------------------------------------*/
+
+/*=========================================================  INCLUDE FILES  ==*/
+
+
+/*=========================================================  LOCAL MACRO's  ==*/
+/*======================================================  LOCAL DATA TYPES  ==*/
+/*=============================================  LOCAL FUNCTION PROTOTYPES  ==*/
+/*=======================================================  LOCAL VARIABLES  ==*/
+/*======================================================  GLOBAL VARIABLES  ==*/
+/*============================================  LOCAL FUNCTION DEFINITIONS  ==*/
+/*===========================================  GLOBAL FUNCTION DEFINITIONS  ==*/
+/*================================*//** @cond *//*==  CONFIGURATION ERRORS  ==*/
+/** @endcond *//** @} *//** @} *//*********************************************
+ * END of source_template.c
+ ******************************************************************************/
