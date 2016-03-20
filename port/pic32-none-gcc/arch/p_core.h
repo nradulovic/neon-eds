cf3c798f2978456a77af2b347bc87b87b683da44
Nenad Radulović <nenad.b.radulovic@gmail.com>
2016-03-21 00:48:58 +0100

    New PIC32MX port

---
+++ port/pic32-none-gcc/arch/p_core.h
@@ -0,0 +1,292 @@
+/*
+ * This file is part of Neon.
+ *
+ * Copyright (C) 2010 - 2015 Nenad Radulovic
+ *
+ * Neon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Neon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Neon.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * web site:    http://github.com/nradulovic
+ * e-mail  :    nenad.b.radulovic@gmail.com
+ *//***********************************************************************//**
+ * @file
+ * @author      Nenad Radulovic
+ * @brief       PIC32MX port core
+ *********************************************************************//** @{ */
+
+#ifndef NEON_ARCH_P_CORE_H_
+#define NEON_ARCH_P_CORE_H_
+
+/*=========================================================  INCLUDE FILES  ==*/
+
+#include <stdint.h>
+#include <xc.h>
+
+#include "port/compiler.h"
+#include "base/config.h"
+
+/*===============================================================  MACRO's  ==*/
+
+/**@brief       Specifies bit-width of general purpose registers
+ */
+#define NCPU_DATA_WIDTH                     32u
+
+/**@brief       Specifies data alignment for optimal performance
+ */
+#define NCPU_DATA_ALIGNMENT                 4u
+
+#define NCPU_REG_MAX                        UINT32_MAX
+
+#define NCPU_SIZE_MAX                       UINT32_MAX
+
+#define NCPU_SSIZE_MAX                      INT32_MAX
+
+/**@brief       Specify the number of bits used in ISR priority mask. 
+ */
+#define NCORE_LOCK_LEVEL_BITS               3u
+
+/**@brief       Convert Neon priority level to port code
+ */
+#define NCORE_LOCK_TO_CODE(lock)                                                \
+    ((lock) >> (8 - NCORE_LOCK_LEVEL_BITS))
+
+/**@brief       Convert port code to Neon priority level
+ */
+#define NCORE_CODE_TO_LOCK(code)                                                \
+    ((code) << (8 - NCORE_LOCK_LEVEL_BITS))
+ 
+#define ncore_os_ready(thread)              (void)thread
+
+#define ncore_os_block(thread)              (void)thread
+
+#define ncore_os_should_exit()
+
+#define ncore_os_exit()
+
+/*-------------------------------------------------------  C++ extern base  --*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*============================================================  DATA TYPES  ==*/
+
+/**@brief General purpose registers are 32bit wide.
+ */
+typedef unsigned int ncore_reg;
+
+typedef unsigned int ncpu_size;
+
+typedef   signed int ncpu_ssize;
+
+struct PORT_C_ALIGN(NCPU_DATA_ALIGNMENT) ncore_ref
+{
+	uint32_t					value;
+};
+
+/**@brief       Interrupt context structure
+ * @details     This type is used to declare variable type which will hold
+ *              interrupt context data.
+ */
+struct ncore_lock
+{
+    unsigned int                level;
+};
+
+struct PORT_C_ALIGN(NCPU_DATA_ALIGNMENT) ncore_atomic
+{
+	int32_t						value;
+};
+
+/*======================================================  GLOBAL VARIABLES  ==*/
+/*===================================================  FUNCTION PROTOTYPES  ==*/
+
+
+PORT_C_INLINE_ALWAYS
+uint_fast8_t ncore_log2(
+    ncore_reg                   value)
+{
+    uint_fast8_t                clz;
+
+    clz = __builtin_clz(value);
+
+    return (31u - clz);
+}
+
+
+
+PORT_C_INLINE_ALWAYS
+ncore_reg ncore_exp2(
+    uint_fast8_t                value)
+{
+    return (0x1u << value);
+}
+
+
+
+PORT_C_INLINE_ALWAYS
+void ncore_ref_write(
+    struct ncore_ref *          ref,
+	uint32_t					value)
+{
+	ref->value = value;
+}
+
+
+
+PORT_C_INLINE_ALWAYS
+uint32_t ncore_ref_read(
+	struct ncore_ref *          ref)
+{
+    return (ref->value);
+}
+
+
+
+PORT_C_INLINE_ALWAYS
+void ncore_ref_increment(
+	struct ncore_ref *      	ref)
+{
+    if (ref->value != UINT32_MAX) {
+        ref->value++;
+    }
+}
+
+
+
+PORT_C_INLINE_ALWAYS
+void ncore_ref_decrement(
+	struct ncore_ref *          ref)
+{
+    if (ref->value != 0u) {
+        ref->value--;
+    }
+}
+
+
+
+/*
+ * TODO: Write this thread safe and in assembler.
+ */
+PORT_C_INLINE_ALWAYS
+void ncore_atomic_write(struct ncore_atomic * v, int32_t i)
+{
+	v->value = i;
+}
+
+
+
+/*
+ * TODO: Write this thread safe and in assembler.
+ */
+PORT_C_INLINE_ALWAYS
+int32_t ncore_atomic_read(
+	struct ncore_atomic *       ref)
+{
+    return (ref->value);
+}
+
+
+
+/*
+ * TODO: Write this thread safe and in assembler.
+ */
+PORT_C_INLINE_ALWAYS
+void ncore_atomic_inc(
+	struct ncore_atomic *      	ref)
+{
+    if (ref->value != INT32_MAX) {
+    	ref->value++;
+    }
+}
+
+
+
+/*
+ * TODO: Write this thread safe and in assembler.
+ */
+PORT_C_INLINE_ALWAYS
+void ncore_atomic_dec(
+	struct ncore_atomic *        ref)
+{
+    if (ref->value != 0u) {
+    	ref->value--;
+    }
+}
+
+
+
+PORT_C_INLINE
+void ncore_lock_enter(
+    struct ncore_lock *          lock)
+{
+#if (CONFIG_CORE_LOCK_MAX_LEVEL != 255)
+    unsigned int                new_mask;
+	unsigned int				ipl_status;
+
+    new_mask = NCORE_LOCK_TO_CODE(CONFIG_CORE_LOCK_MAX_LEVEL);
+    new_mask <<= _CP0_STATUS_IPL_POSITION;
+
+    ipl_status  = _CP0_GET_STATUS();
+    lock->level = ipl_status;
+    ipl_status &= ~_CP0_STATUS_IPL_MASK;
+    ipl_status |= new_mask;
+    _CP0_SET_STATUS(ipl_status);
+#else
+	__asm __volatile__(
+		"   di      %0                                  \n"
+		: "=r"(lock->level)
+        : "memory"
+        :);
+#endif
+}
+
+
+
+PORT_C_INLINE
+void ncore_lock_exit(
+    struct ncore_lock *          lock)
+{
+#if (CONFIG_CORE_LOCK_MAX_LEVEL != 255)
+    unsigned int                new_mask;
+	unsigned int				ipl_status;
+
+    new_mask = NCORE_LOCK_TO_CODE(CONFIG_CORE_LOCK_MAX_LEVEL);
+    new_mask <<= _CP0_STATUS_IPL_POSITION;
+
+    ipl_status  = _CP0_GET_STATUS();
+    ipl_status &= ~_CP0_STATUS_IPL_MASK;
+    ipl_status |= lock->level & _CP0_STATUS_IPL_MASK;
+    _CP0_SET_STATUS(ipl_status);
+#else
+    unsigned int                isr_status;
+    
+    if (lock->level) {
+        __asm __volatile__(
+            "   ei                                      \n"
+            :
+            : "memory"
+            :);        
+    }
+#endif
+}
+
+/*--------------------------------------------------------  C++ extern end  --*/
+#ifdef __cplusplus
+}
+#endif
+
+/*================================*//** @cond *//*==  CONFIGURATION ERRORS  ==*/
+/** @endcond *//** @} *//******************************************************
+ * END of p_core.h
+ ******************************************************************************/
+#endif /* NEON_ARCH_P_CORE_H_ */
